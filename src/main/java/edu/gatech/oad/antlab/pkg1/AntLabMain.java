dc9d23819f63a96ac6f7dd1ea99520e593d23b84
Bowen <bran100@gatech.edu>
2018-01-30 14:32:51 -0500

    Fixed javadoc warning

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -84,6 +84,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 
     /**
      * entry point for the program
+     * @param args a list of arguments
      */
      public static void main(String[] args) {
 
