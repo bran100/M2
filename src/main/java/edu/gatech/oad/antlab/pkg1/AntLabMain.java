d23672879179bb0eca8ce872e21435524d0859f1
Samantha Mann <srmann96@yahoo.com>
2018-01-31 15:44:31 -0500

    Person 6 added name to file

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -75,6 +75,10 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
                   toPrint += p5.toString("gburdell5");
+                  //Person6 replace P6 with your name
+                  //and gburdell6 with your gt id
+                  Person6 p6 = new Person6("Samantha Mann");
+                  toPrint += p6.toString("903149599");
 
         System.out.println(toPrint);
 
